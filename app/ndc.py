940ae9d68024f0ce21a9d3d8e150a76dc2d5ea88
Cavernos <louisdescavernes@gmail.com>
2023-05-25 11:05:41 +0200

    Add Animation

--- app/ndc.py
+++ app/ndc.py
@@ -1,19 +1,20 @@
 import pyxel
 
 class Sprite:
-    def __init__(self, resources, x, y):
-        self.resources = resources
+    def __init__(self, x, y):
         self.x, self.y = x, y
         self.width, self.height = 8,8
 
     def update(self):
-        pass
+        if pyxel.btnp(pyxel.KEY_D):
+            self.x = self.x + 1
+
     def draw(self):
         pyxel.blt(self.x, self.y, 0, 24, 16, self.width, self.height)
 
 class Hero(Sprite):
-    def __init__(self, resources, x, y):
-        super().__init__(resources, x, y)
+    def __init__(self, x, y):
+        super().__init__(x, y)
 
 class IA(Sprite):
     def __init__(self):
@@ -31,11 +32,13 @@ class App:
     def __init__(self):
         pyxel.init(128, 128)
         self.resources = pyxel.load("..\\assets\\2.pyxres")
-        self.hero = Hero(self.resources, 0, 0)
+        self.hero = Hero(0, 0)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        if pyxel.btnp(pyxel.KEY_ESCAPE):
+            pyxel.quit()
+        self.hero.update()
     def draw(self):
         pyxel.cls(0)
         self.hero.draw()
